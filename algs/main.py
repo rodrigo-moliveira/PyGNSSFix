c80131d74c773e56569cc841e5065ad872210fc8
rooo <rooo@elecnor.es>
2023-02-10 10:35:10 +0000

    feat: Adding ``algs`` directory with main scripts

---
+++ algs/main.py
@@ -0,0 +1,17 @@
+import sys
+
+# do Imports as
+from src.errors import EopError
+
+
+def main():
+    pass
+
+
+
+print("#--------------------------------------------------#")
+print("#           Welcome to PyGNSSFix Program           #")
+print("#--------------------------------------------------#\n")
+
+if __name__ == "__main__":
+    main()
